e2bf8520ed9cf7c4f991dd6c7358cb659c24ed87
Haaris <89451392+haaris272k@users.noreply.github.com>
2023-09-08 10:50:17 +0530

    add: error handling

--- Wallomator/execute.bat
+++ Wallomator/execute.bat
@@ -3,7 +3,7 @@ echo "Executing the Python script to change wallpapers..."
 
 REM Define the full path to your Python script.
 REM Modify the path below to match the location of your script.
-set SCRIPT_PATH=D:\Python\Projects\scripts\wallpaper\script.py
+set SCRIPT_PATH=C:\Users\dir1\dir2\dir3\wallomator\Wallomator\script.py
 
 REM Check if the Python script exists at the specified path.
 if not exist "%SCRIPT_PATH%" (
@@ -12,8 +12,8 @@ if not exist "%SCRIPT_PATH%" (
   exit /b 1
 )
 
-REM Run the Python script.
-python "%SCRIPT_PATH%"
+REM Run the Python script silently, redirecting output and error to NUL.
+python "%SCRIPT_PATH%" > NUL 2>&1
 
 echo "Script execution complete."
 pause
